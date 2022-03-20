2ac186a220871152d0d36d21af0aa204103c2d9f
vaishnaviteegala <vaishnaviteegala12@gmail.com>
2022-03-20 18:18:39 +0530

    added json

---
+++ javascript/json-objects.js
@@ -0,0 +1,11 @@
+// Today's topic: JSON objects:
+
+
+// here abc is an object
+const abc = {
+	x: 23,
+  y: 25,
+  def: [ 1, 10, 11, "aditya's pc ", { a: 1 }, true ] // def is an array
+} // this is json object
+
+console.log(abc.def[4].a)
--- javascript/test.js
+++ javascript/test.js
@@ -1,3 +1,10 @@
 console.log('hello')
 const y='world'
 console.log(y)
+/*
+git hub usage:
+1. create repository/folder/package in github website
+2. copy created repository clone link
+3. clone/download it in local computer using git  clone command
+4. start using the repository
+*/
